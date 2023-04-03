719171287a5f6bca82172f4d4e621598136e97a3
aloha <aloha@LAPTOP-04PILRAK.lan>
2023-04-03 19:42:53 +1000

    Third commit: Created columns, image and description

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.png')
+
+with col2:
+    title = """
+    Hi, I'm the TheZOnn
+    """
+    st.title(title)
+    content = """
+    I also go by the names: Verizon, Verizonn and Zazoon.
+    I'm a little goober that goofs around the internet sometimes. I created this website so it could be some random 
+    portfolio that I doubt I would use in the future.
+    """
+    st.info(content)
