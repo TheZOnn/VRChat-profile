537a4fd1e85784672056c77faf491469162b1e36
TheZOnn <125360437+TheZOnn@users.noreply.github.com>
2023-04-04 22:37:57 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,7 +31,7 @@ col3, col4 = st.columns(2)
 
 with col3:
     st.image('vrmoments/vr1.PNG')
-    st.image('vrmoments/vr2.PNG')
+    st.image('vrmoments/vr2.png')
 
 with col4:
     st.image('vrmoments/vr3.PNG', width=442)
